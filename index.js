dfe61efe7bd97a2841d7d02bd6412b6e756ae1d8
uustoboy <tianqi_email@163.com>
2019-12-29 20:50:37 +0800

    add

--- index.js
+++ index.js
@@ -8,90 +8,26 @@ const extname = path.extname;
 const logger = log4js.getLogger();
 logger.level = 'debug';
 
-// input your path
-// const inputPath = "/Users/uustoboy/web/git/TxtReplace/";
-// const inputPath = "E:/cece2/c/index1.css";
-// const inputPath = "E:/cece2";
-const inputPath = [
-    "E:/cece2/c/index1.css",
-    "E:/cece2/c/index2.css",
-    "E:/cece2/c/index3.css"
+//文件路径(可字符串，可数组):支持文件夹;
+let inputPath = [
+  "/Users/uustoboy/web/git/TxtReplace/public.css",
+  "/Users/uustoboy/web/git/TxtReplace/public1.css",
+  "/Users/uustoboy/web/git/TxtReplace/public2.css",
+  "/Users/uustoboy/web/git/TxtReplace/aa/"
 ];
-// const filePath = resolve(inputPath);
+
+//let inputPath = "/Users/uustoboy/web/git/TxtReplace/public2.css";
 
 //限定文件后缀;
-let suffix= ['.css'];
+let suffix = ['.css','.js'];
 
 //全局替换文本;
-let textReplace={
-    text: 'http://',
-    replace: '//'
-}
+let textReplace = {
+    'http://': '//',
+    'background': 'bg'
+};
 
-
-// 执行方法;
-// fileDisplayPath(filePath);
-
-/**
- * 文件遍历
- * @param fileDisplayPath
- */
-// function fileDisplayPath(filePath) {
-//     fs.readdir(filePath, (err, files) => {
-//         if (err) {
-//             logger.debug(err)
-//         } else {
-//             files.forEach((filename) => {
-//                 // 获取绝对路径
-//                 let filedir = path.join(filePath, filename)
-
-//                 fs.stat(filedir, (error, stats) => {
-//                     if (error) {
-//                         logger.debug(error)
-//                     } else {
-//                         // 文件夹、文件的不同处理
-//                         let isFile = stats.isFile()
-//                         let isDir = stats.isDirectory()
-
-//                         if (isFile && suffix.includes(extname(filedir))) {
-//                             let content = fs.readFileSync(filedir, "utf-8");
-//                             let strNew = content.replace(textReplace.text, textReplace.replace);
-//                             fs.writeFile(filedir, strNew, "utf8", err => {
-//                               if (err) throw err;
-//                               logger.info("success done");
-//                               let fileTxt = __dirname +'TxtReplace.txt';
-//                                 if (isFileExisted(fileTxt)){
-
-//                                 }else{
-//                                     let txt = filedir +'\n';
-//                                     fs.writeFile(
-//                                       fileTxt,
-//                                         txt,
-//                                       function(err) {
-//                                         if (err) {
-//                                           return console.log(err);
-//                                         }
-//                                         console.log("The file was saved!");
-//                                       }
-//                                     );
-//                                 }
-
-//                                 // let content = fs.readFileSync(filedir, "utf-8");
-//                                 // fs.writeFile(,'');
-//                             });
-//                         }
-
-//                         if (isDir) {
-//                             // 递归
-//                             fileDisplayPath(filedir)
-//                         }
-//                     }
-//                 })
-//             })
-//         }
-//     })
-// }
-
+//替换文件;
 const filterFile = (parameter)=>{
     if( getType(parameter) =='Array' ){
         parameter.forEach((value, index, array)=>{
@@ -100,39 +36,52 @@ const filterFile = (parameter)=>{
     }else if(getType(parameter) =='String'){
         let  statType = isFileExisted(parameter);
         if( statType == '0' && suffix.includes(extname(parameter)) ){
-            let content = readFile(parameter);                     
-            let strNew = content.replaceAll(textReplace.text, textReplace.replace);
-            // console.log(strNew)
+            let content = readFile(parameter);
+            let keyArr = [];
+            for (var key in textReplace) {
+                keyArr.push(key);
+            }
+            reg = new RegExp(keyArr.join("|"), "g");
+            let strNew = content.replace(reg, function(matchStr) {
+              return textReplace[matchStr];
+            });
+
              if(strNew.toString()!==content.toString()){
+                writeLog(parameter);
                 writeFile(parameter,strNew,'更改成功！');
              }
-            
         }else if(statType=='1'){
             fileDisplayPath(parameter).then(files => {
                 files.forEach((filename) => {
                     // 获取绝对路径
-                    let filedir = join(parameter, filename)
-                    // console.log(filedir)
+                    let filedir = join(parameter, filename);
                     fs.stat(filedir, (error, stats) => {
-                        if (error) {
-                            logger.debug(error);
-                        } else {
-                            // 文件夹、文件的不同处理
-                            let isFile = stats.isFile();
-                            let isDir = stats.isDirectory();
-                            if (isFile && suffix.includes(extname(filedir))) {
-                                let content = readFile(filedir);
-                                let strNew = content.replace(textReplace.text, textReplace.replace);
-                                if(strNew !== content){
-                                    writeFile(parameter,strNew,'更改成功！');
-                                }
-                                
+                      if (error) {
+                        logger.debug(error);
+                      } else {
+                        // 文件夹、文件的不同处理
+                        let isFile = stats.isFile();
+                        let isDir = stats.isDirectory();
+                        if (isFile && suffix.includes(extname(filedir))) {
+                            let content = readFile(filedir);
+                            let keyArr = [];
+                            for (var key in textReplace) {
+                                keyArr.push(key);
                             }
-                            if (isDir) {
-                                // 递归
-                                filterFile(filedir);
+                            reg = new RegExp(keyArr.join("|"), "g");
+                            let strNew = content.replace(reg, function(matchStr) {
+                                return textReplace[matchStr];
+                            });
+                            if (strNew.toString() !== content.toString()) {
+                                writeLog(filedir);
+                                writeFile(filedir, strNew, "更改成功！");
                             }
                         }
+                        if (isDir) {
+                          // 递归
+                          filterFile(filedir);
+                        }
+                      }
                     });
                 });
             })
@@ -142,31 +91,27 @@ const filterFile = (parameter)=>{
 
 //读取文件;
 const  readFile =  (filedir)=>{
-    let content =  fs.readFileSync(filedir, "utf-8");
+    let content = fs.readFileSync(filedir, "utf-8");
     return content;
 }
 
-//写入日志;
-const writeLog = (filedir) =>{
-    let fileTxt = join(__dirname,'TxtReplace.txt');
-    if( isFileIn(fileTxt) ){
-        let content = readFile(fileTxt);
-        let strNew = content+`${filedir} \n`;
-        writeFile(fileTxt,strNew,'写入日志~',false);
-    }else{
-        let txt = filedir +'\n';
-        writeFile(fileTxt,txt,'写入日志~',false);
-    }
+//更改文件;
+const writeFile = (parameter,strNew,title) =>{
+    fs.writeFile(parameter, strNew, "utf8", err => {
+      if (err) logger.debug(err);
+        logger.debug(`${parameter} ${title}`);
+    });
 }
 
-//更改文件;
-const writeFile = (parameter,strNew,title,judgeLog=true) =>{
-    fs.writeFile(parameter, strNew, "utf8", err => {
-        if (err)  logger.debug(err);
-        logger.info(`${parameter} ${title}`);
-        if(judgeLog){
-            writeLog(parameter);
-        }
+//写入日志;
+const writeLog = (filedirArr) => {
+    let fileTxt = join(__dirname, 'TxtReplace.txt');
+    let content = isFileIn(fileTxt) ? readFile(fileTxt) : '';
+    let txt = content + "\n" + filedirArr ;
+    logger.info(`${filedirArr} 写入日志`);
+    fs.writeFileSync(fileTxt, txt, "utf8", err => {
+        if (err) logger.debug(err);
+        logger.info(`${fileTxt} 写入日志`);
     });
 }
 
@@ -175,7 +120,7 @@ const fileDisplayPath = (filePath)=>{
     return new Promise((resolve, reject) => {
         fs.readdir(filePath, (err, files) => {
             if(err){
-                return  reject(err); 
+                return  reject(err);
             }else{
                 return  resolve(files);
             }
@@ -215,4 +160,5 @@ String.prototype.replaceAll = function(s1, s2) {
     return this.replace(new RegExp(s1, "gm"), s2);
 }
 
-filterFile(inputPath)
+// 执行方法;
+filterFile(inputPath);
